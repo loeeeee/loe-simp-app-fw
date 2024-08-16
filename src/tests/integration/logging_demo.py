e445ec7b844d0fe8197e58196eb54f6f137f2a09
loeeeee <95266635+loeeeee@users.noreply.github.com>
2024-08-16 17:32:29 +0800

    move test around

--- src/tests/integration/logging_demo.py
+++ src/tests/integration/logging_demo.py
@@ -1,28 +1,29 @@
 from ...loe_simp_app_fw import Logger
 import time
 
-def logging() -> None:
+def logging_no_error() -> None:
     Logger.debug("This is a debug message")
     Logger.info("This is a info message")
     Logger.warning("This is a warning message")
-    Logger.error("This is a error message")
 
 def main() -> None:
-    logging()
+    logging_no_error()
     print("BOOTSTRAP")
     Logger.bootstrap("./log")
     print("Finish Bootstrap")
-    logging()
-    time.sleep(5)
+    for _ in range(200):
+        logging_no_error()
+    Logger.error("This is a error message")
+    time.sleep(3)
     print("Finish logging")
     Logger._debootstrap()
     print("Stopped")
 
     # -----------------------------------------------------
-    logging()
+    logging_no_error()
     print("Force single process logger")
     Logger.bootstrap("./log", isMultiprocessing=False)
-    logging()
+    logging_no_error()
     print("Finish logging")
 
 if __name__ == "__main__":
